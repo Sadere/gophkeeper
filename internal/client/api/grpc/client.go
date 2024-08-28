36b8a7401e2dbeac715bb104c4330740c864a6b4
Ilya Marchenko <sasa.mie96@gmail.com>
2024-08-29 00:26:25 +0300

    client: don't show error if no secrets added

--- internal/client/api/grpc/client.go
+++ internal/client/api/grpc/client.go
@@ -18,8 +18,10 @@ import (
 	"github.com/Sadere/gophkeeper/internal/client/api/grpc/interceptor"
 	"github.com/Sadere/gophkeeper/internal/client/config"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
@@ -185,6 +187,12 @@ func (c *GRPCClient) LoadPreviews(ctx context.Context) (model.SecretPreviews, er
 
 	response, err := c.secretsClient.SecretPreviewsV1(ctx, &emptypb.Empty{})
 	if err != nil {
+		if e, ok := status.FromError(err); ok {
+			// Return empty list if no secrets added yet
+			if e.Code() == codes.NotFound {
+				return nil, nil
+			}
+		}
 		return nil, fmt.Errorf("failed to retrieve secrets: %w", err)
 	}
 
