23b83af21acc6e98607d5a888fe2a353ea475ac3
Ilya Marchenko <sasa.mie96@gmail.com>
2024-09-09 01:03:45 +0300

    grpc server additional tests

--- internal/server/grpc/server_test.go
+++ internal/server/grpc/server_test.go
@@ -7,6 +7,7 @@ import (
 	service "github.com/Sadere/gophkeeper/internal/server/service/mocks"
 	"github.com/Sadere/gophkeeper/internal/server/utils"
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 )
 
 func NewTestServer(t *testing.T) (*KeeperServer, *service.MockIUserService, *service.MockISecretService, *gomock.Controller, *gomock.Controller) {
@@ -35,4 +36,45 @@ func NewTestServer(t *testing.T) (*KeeperServer, *service.MockIUserService, *ser
 		secretMock,
 		userCtrl,
 		secretCtrl
+}
+
+func TestServer(t *testing.T) {
+	server, _, _, userCtrl, secretCtrl := NewTestServer(t)
+	defer func() {
+		userCtrl.Finish()
+		secretCtrl.Finish()
+	}()
+
+	t.Run("register no tls", func(t *testing.T) {
+		_, err := server.Register()
+
+		assert.NoError(t, err)
+	})
+
+	t.Run("register tls", func(t *testing.T) {
+		server.config.EnableTLS = true
+		_, err := server.Register()
+
+		assert.NoError(t, err)
+	})
+}
+
+func TestLoadTLSConfig(t *testing.T) {
+	t.Run("failed to read ca cert", func(t *testing.T) {
+		_, err := loadTLSConfig("invalid", "", "")
+
+		assert.EqualError(t, err, "failed to read CA cert: open invalid: file does not exist")
+	})
+
+	t.Run("failed to read server cert", func(t *testing.T) {
+		_, err := loadTLSConfig("ca-cert.pem", "invalid", "")
+
+		assert.EqualError(t, err, "failed to read server cert: open invalid: file does not exist")
+	})
+
+	t.Run("failed to read server key", func(t *testing.T) {
+		_, err := loadTLSConfig("ca-cert.pem", "server-cert.pem", "invalid")
+
+		assert.EqualError(t, err, "failed to read server key: open invalid: file does not exist")
+	})
 }
